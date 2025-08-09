564bc9cc42d005db632cde149bb1fcb9dea4eb15
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2025-08-09 16:52:52 +0200

    Step 1: Add mongoose connection

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
+let mongoose;
+try {
+  mongoose = require("mongoose");
+} catch (e) {
+  console.log(e);
+}
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
