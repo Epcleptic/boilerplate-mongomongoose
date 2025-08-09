953e85996a879c75a451a6b30ed6ae7cb54afa57
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2025-08-09 18:34:09 +0200

    Step 8: Bug

--- myApp.js
+++ myApp.js
@@ -78,7 +78,7 @@ const findEditThenSave = (personId, done) => {
 
   Person.findOne({ _id: personId }, function (err, person) {
     if (err) return console.error(err);
-    person.favoriteFoods += [foodToAdd];
+    person.favoriteFoods.push(foodToAdd);
     person.save(function (err, data) {
       handleData(done, err, data);
     });
