8efa22fc739b49664a9e95c7e8be3ceab5edb148
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2025-08-09 18:51:38 +0200

    Step 12: Implement queryChain

--- myApp.js
+++ myApp.js
@@ -132,7 +132,11 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ food: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select({ name: true, age: false, favoriteFoods: true })
+    .exec(done(err, data));
 };
 
 /** **Well Done !!**
